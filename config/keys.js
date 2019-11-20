988630da8e06e19658b4204bfb87d0eea736c72b
fady_wasfalla <fady.wasfalla@gmail.com>
2019-11-20 18:41:52 +0200

    start

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const user = 'monica.fayez%40student.guc.edu.eg'
+const password = 'mongodb#2019'
+module.exports ={
+    mongoURI:'mongodb+srv://fady:Smsm2512@mypet-uqkhl.mongodb.net/test?retryWrites=true&w=majority'
+}
