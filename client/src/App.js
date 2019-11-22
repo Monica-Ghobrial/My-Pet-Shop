439de2edd5322f90bdfcc8d259032ce7a8574663
fady_wasfalla <fady.wasfalla@gmail.com>
2019-11-23 00:01:49 +0200

    sign up

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import SignIn from './pages/signIn'
-//import SignUp from './pages/signUp'
+import SignUp from './pages/signUp'
 
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
           integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
           crossOrigin='anonymous' />
         <Switch>
-          {/* <Route exact path='/signUp' component={SignUp} /> */}
+          <Route exact path='/signUp' component={SignUp} />
         
         </Switch>
 
--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Form , Col , Row , Card, Button , Collapse} from "react-bootstrap";
-//import Select from "react-select"
-
+import Select from "react-select"
+import PhoneInput from 'react-phone-number-input/input'
+import SmartInput from 'react-phone-number-input/smart-input'
 
 
 class signUp extends Component {
@@ -9,8 +10,8 @@ class signUp extends Component {
     state = {
        firstName:"",
        lastName:"",
+       email:"",
        phoneNumber:"",
-       emaai:"",
        address:"",
        gender:"",
 
@@ -21,16 +22,23 @@ class signUp extends Component {
           return true
         }
         return false
+    }
+    
+    submitData =(event)=>{
+        event.preventDefault();
+       console.log(this.state)
       }
+
      
-      render() {
+    render() {
        
         return (
             <React.Fragment>
-                <Form>
+                <Form onSubmit={this.submitData}>
                 <Col md={3}>
                 <Card border="secondary">
                 <Col md={12}>
+                    <br/>
                     <Form.Row>
                         <Form.Group as={Col} md={{span:9}} >
                             <Form.Label>First Name<span style={{color:"red"}}>✶</span></Form.Label>
@@ -55,27 +63,45 @@ class signUp extends Component {
                     <Form.Row>
                         <Form.Group as={Col} md={{span:9}} >
                             <Form.Label>Phone Number<span style={{color:"red"}}>✶</span></Form.Label>
-                            <Form.Control as="textarea" rows="1" required onChange={(e)=>{this.setState({phoneNumber:e.target.value})}} />
+                            <PhoneInput
+                            required
+                            placeholder="Enter phone number"
+                            country="EG"
+                            value={ this.state.phoneNumber }
+                            onChange={ (e) => this.setState({ phoneNumber:e }) }/>
                         </Form.Group>
                     </Form.Row>
 
                     <Form.Row>
-                        <Form.Group as={Col} controlId=">CustomerType" >
+                        <Form.Group as={Col} md={{span:9}} >
+                            <Form.Label>Address<span style={{color:"red"}}>✶</span></Form.Label>
+                            <Form.Control as="textarea" rows="1" required onChange={(e)=>{this.setState({address:e.target.value})}} />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group as={Col} md={{span:9}}>
                             <Form.Label>Gender</Form.Label>
-                           {/* <Select
+                           <Select
                            value={this.state.gender.value}
                            onChange={(e)=>{this.setState({ gender: e.value})}}
                            options={[
                                        { value: 'Male', label: 'Male' },
                                        { value: 'Female', label: 'Female' },
                                    ]}
-                           /> */}
+                           />
                               {/* <input tabIndex={-1} autoComplete="off" style={{ opacity: 0, height: 0 }}
                                 required={this.validateItem(this.state.gender)}/> */}
                             
                         </Form.Group>
                     </Form.Row>
 
+                    <Form.Row>
+                        <Form.Group as={Col} md={{offset:4}}>
+                        <Button type="submit">Submit</Button>
+                        </Form.Group>
+                    </Form.Row>
+                    <br/>
                 </Col>
                 </Card>
                 </Col>
